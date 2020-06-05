73fdd0ba33f309ecb3aa0adb2bb5dcf0ddb1ea69
Lucas Tiago <eulukasthyago@outlook.com>
2020-06-05 19:15:51 -0300

    Updates

---
+++ precache-manifest.dc3fd2770e0252b6ef815ad79955aae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22274854ac795829f0133f8b97e2dad4",
+    "url": "/anifoxpage/index.html"
+  },
+  {
+    "revision": "bf307d88523cc8b9a2a2",
+    "url": "/anifoxpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa21f8458ad8c5986b65",
+    "url": "/anifoxpage/static/js/2.d4538808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anifoxpage/static/js/2.d4538808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf307d88523cc8b9a2a2",
+    "url": "/anifoxpage/static/js/main.154cecfd.chunk.js"
+  },
+  {
+    "revision": "d09150fb58d835b71809",
+    "url": "/anifoxpage/static/js/runtime-main.06fd1620.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anifoxpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
